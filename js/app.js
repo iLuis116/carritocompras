7a7fe2131471aa9543b495b725b302c9431e1a72
Jose Luis Nava Flores <luisflores2103@gmail.com>
2022-10-03 11:03:26 -0500

    Commit 2 en clase

--- js/app.js
+++ js/app.js
@@ -4,15 +4,28 @@ const listaCursos = document.querySelector("#lista-cursos");
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
 const vaciarCarrito = document.querySelector("#vaciar-carrito");
 
+let listadoCarrito = [];
 
 const agregarCurso = (e) => {
     e.preventDefault();
     if( e.target.classList.contains("agregar-carrito")){
-        console.log("Presionaste el botón.");
+        const curso = e.target.parentElement.parentElement;
+        const infoCurso = {
+            imagen: curso.querySelector('img').src,
+            nombre: curso.querySelector('h4').textContent,
+            precio: curso.querySelector('p.precio').textContent,
+            id: curso.querySelector('.agregar-carrito').getAttribute('data-id'),
+            cantidad: 1
+        }
+        console.log(infoCurso);
+        agregarCarrito(infoCurso);
     }
 }
 
-
+const agregarCarrito = curso => {
+    listadoCarrito = [...listadoCarrito, curso]
+    console.log(listadoCarrito);
+}
 const cargarEventListiner = () => {
     //funciones
     listaCursos.addEventListener("click", agregarCurso);
