5ef614cbcf50fa1122f3f814c7cfe71a065e1bc3
Jose Luis Nava Flores <luisflores2103@gmail.com>
2022-10-03 11:42:50 -0500

    Funcion de carrito en la pagina

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const carrito = document.querySelector("#carrito");
 const listaCursos = document.querySelector("#lista-cursos");
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
-const vaciarCarrito = document.querySelector("#vaciar-carrito");
+const BTNvaciarCarrito = document.querySelector("#vaciar-carrito");
 
 let listadoCarrito = [];
 
@@ -25,7 +25,30 @@ const agregarCurso = (e) => {
 const agregarCarrito = curso => {
     listadoCarrito = [...listadoCarrito, curso]
     console.log(listadoCarrito);
+    generarHTML();
 }
+
+const generarHTML = () =>{
+    vaciarCarrito();
+    listadoCarrito.forEach(curso => {
+        const row = document.createElement('tr');
+        const cursoHTML = `
+        <td>
+           <img src="${curso.imagen}"width=100>
+        </td>
+        <td>${curso.nombre}</td>
+        <td>${curso.precio}</td>
+        <td>${curso.cantidad}</td>
+        `;
+        row.innerHTML = cursoHTML;
+        contenedorCarrito.appendChild(row);
+    });
+}
+
+const vaciarCarrito = () => {
+    contenedorCarrito.innerHTML = '';
+}
+
 const cargarEventListiner = () => {
     //funciones
     listaCursos.addEventListener("click", agregarCurso);
